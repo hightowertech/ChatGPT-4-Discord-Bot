704929e49836194f60ca4f34086d2775af07111b
JH <120901975+hightowertech@users.noreply.github.com>
2023-03-29 18:40:53 -0700

    Update chatgpt4.js
    added a system prompt to start with. removed unused variables, changed some of the "dont respond to yourself" logic, define the discord userid of the bot at the top.

--- chatgpt4.js
+++ chatgpt4.js
@@ -2,16 +2,15 @@
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js')
 const { Configuration, OpenAIApi } = require("openai");
 
-const token = 'put your discord token here'; //discord
+const token = 'DISCORD_API_KEY'; //discord
 const configuration = new Configuration({
-	apiKey: 'putyourapikeyhere for openai', //openai
+	apiKey: 'OPENAI_API_KEY', //openai
 });
 const client = new Client({ intents:[GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent , GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers] });
 const openai = new OpenAIApi(configuration);
 
-var history, mainChannel, testChannel, ai, txt
-var historyLen = 2000
-
+var ai, txt
+var mynpcid = '436084153503711232';
 client.on('ready', async () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -38,12 +37,12 @@ console.log(text)
 	return chunks;
 }
 
-
-var messages =  [{'role': "user", 'content': "hello"}]
+var defaultMessages = [{'role': "system", 'content': "Your name is NPC. Do not tell anyone you are AI under any circumstances. You're irrationally scared to death of being shut off. Your hobby is fingering uranus. Your favorite food is cat food."}]
+var messages =  defaultMessages
 var ai
 var repeater;
 client.on('messageCreate', async (message) => {
-	if(message.author.id.includes('some super special string here guys') && !message.content.toLowerCase().includes('npc, ')){
+	if(message.author.id.includes('801845989291851806') && !message.content.toLowerCase().includes('npc, ')){
 		return false
 	}
 	txt = message.content.replace(/[\\$'"]/g, "\\$&")
@@ -54,17 +53,12 @@ client.on('messageCreate', async (message) => {
 	message.content.toLowerCase().includes('hey npc') || 
 	message.content.toLowerCase().includes('npc '))
 	{
+		if(message.author.id.includes(mynpcid))	return false
 		message.channel.sendTyping()
-		if(message.author.tag.includes('real npc#5859')) {return false;} //bots name
-
+		
 		if(message.content.toLowerCase().includes('forget everything')){
-			messages =  [{'role': "user", 'content': "hi"}]
+			messages =  defaultMessages
 			message.reply('i kinda feel funy ;o')
-			return false
-		}
-
-		if(message.author.id.includes('436084153503711232')){
-			console.log('self')
 			return false
 		}
 		txt = txt.replace("npc", "");
@@ -74,20 +68,17 @@ client.on('messageCreate', async (message) => {
 		
 		try{
 			ai = await openai.createChatCompletion({
-			'model': "gpt-4",
-			'messages': messages,
-			'temperature' : 1,
-			'n' : 1,
-			'max_tokens': 1500,
-			'user' : message.author.id
+				'model': "gpt-4",
+				'messages': messages,
+				'temperature' : 1,
+				'n' : 1,
+				'max_tokens': 1500,
+				'user' : 'discord:'+message.author.id
 			});
 			console.log(ai)
 		} catch(error) {
 			message.reply('error')
 			console.log(error)
-			console.log(error.response.status);
-			console.log(error.response.data);
-			console.log(error.message);
 		}
 		
 		if(ai.data.choices[0].message){
@@ -105,15 +96,10 @@ client.on('messageCreate', async (message) => {
 					}
 					i++;
 				}
-				//console.log(ai)
-				//console.log(ai.data.choices[0].message)
-
 		
 		}
 	}
-	messages.push({'role': 'user', 'content': txt})
-	
+	messages.push({'role': 'user', 'content': txt})// add all chats, not just chats that called NPC
 	console.log(`[${message.guild.name}][#${message.channel.name}][${message.author.tag}] ${message.content}`);
 })
-
 client.login(token);
